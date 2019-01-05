3f8ddbef4a83f5f25f2f1910f06c1d8b4484f6f9
Mozgin Vladimir <mozgin.vova@gmail.com>
2019-01-05 13:08:23 +0200

    I1 | Setup Rails with React

---
+++ app/javascript/packs/thankYou/components/landingPage.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+import {Api} from "../middleware/api"
+
+class LandingPage extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Thank You</h1>
+      </div>
+    )
+  }
+}
+export default LandingPage
