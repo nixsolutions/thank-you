661f228f8ca478f1ab91a161f08ba7c32ed33d1d
Vladimir-Kuchinskiy <kuchinskiy.wladimir@gmail.com>
2019-01-29 15:51:21 +0200

    Fixes #19 Add devise authentication and feature specs

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 class PagesController < ApplicationController
-  def root; end
+  def index; end
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,13 @@
 # frozen_string_literal: true
 
 FactoryBot.define do
+  sequence :email do |n|
+    "user#{n}@test.com"
+  end
+
   factory :user do
+    email
+    password { '12345678' }
+    password_confirmation { '12345678' }
   end
 end
