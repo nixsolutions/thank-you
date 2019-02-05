7c5ecc8702cba5c134451d1029250ce1f2dd6af0
quantumInverter <blackhand07@gmail.com>
2019-02-05 13:42:00 +0200

    #I20 Create Custom Validator for Contact

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -5,14 +5,5 @@ class Contact < ApplicationRecord
   belongs_to :contact_type
 
   validates :contact_type, :data, :organization, presence: true
-  validate :format_of_data
-
-  private
-
-  def format_of_data
-    return unless data && contact_type
-
-    error_message = I18n.t('activerecord.errors.models.contact.attributes.data.informat')
-    errors.add(:data, error_message) unless data =~ contact_type.regex
-  end
+  validates :data, contact_data: true
 end
