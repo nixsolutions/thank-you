6407e35b254d47861e4e5bcd73933f4e2a33710f
TheBlackArroVV <theblackarrovv@gmail.com>
2019-03-15 14:18:00 +0200

    Fixes #44 Add locales to controller tests

--- spec/controllers/contacts_controller_spec.rb
+++ spec/controllers/contacts_controller_spec.rb
@@ -15,23 +15,23 @@ describe 'Contacts Controller', type: :request do
 
     context 'when valid data' do
       before do
-        post '/contacts', params: { contact: data }
+        post '/en/contacts', params: { contact: data }
       end
 
       it 'creates a new contact' do
         expect do
-          post '/contacts', params: { contact: data }
+          post '/en/contacts', params: { contact: data }
         end.to change(Contact, :count).by(1)
       end
     end
 
     context 'when invalid data' do
       it 'not create a new contact' do
-        expect { post '/contacts', params: { contact: { data: 'data' } } }.not_to change(Contact, :count)
+        expect { post '/en/contacts', params: { contact: { data: 'data' } } }.not_to change(Contact, :count)
       end
 
       it 'will render new' do
-        post '/contacts', params: { contact: { data: 'data' } }
+        post '/en/contacts', params: { contact: { data: 'data' } }
         expect(response).to render_template :new
       end
     end
@@ -41,20 +41,20 @@ describe 'Contacts Controller', type: :request do
     let!(:contact) { create :contact }
 
     it 'will set variable' do
-      patch "/contacts/#{contact.id}", params: { contact: { data: '+380505555559' } }
+      patch "/en/contacts/#{contact.id}", params: { contact: { data: '+380505555559' } }
       expect(assigns(:contact)).to eq contact
     end
 
     context 'when valid data' do
       it 'update contact' do
-        patch "/contacts/#{contact.id}", params: { contact: { data: '+380505555559' } }
+        patch "/en/contacts/#{contact.id}", params: { contact: { data: '+380505555559' } }
         contact.reload
         expect(contact.data).to eq '+380505555559'
       end
     end
 
     context 'when invalid data' do
-      before { patch "/contacts/#{contact.id}", params: { contact: { data: '' } } }
+      before { patch "/en/contacts/#{contact.id}", params: { contact: { data: '' } } }
 
       it 'don`t save data to db' do
         contact.reload
@@ -67,11 +67,11 @@ describe 'Contacts Controller', type: :request do
     let!(:contact) { create :contact }
 
     it 'change count in db' do
-      expect { delete "/contacts/#{contact.id}" }.to change(Contact, :count).by(-1)
+      expect { delete "/en/contacts/#{contact.id}" }.to change(Contact, :count).by(-1)
     end
 
     it 'redirects to all organizations' do
-      delete "/contacts/#{contact.id}"
+      delete "/en/contacts/#{contact.id}"
 
       expect(response).to redirect_to organization_path(contact.organization)
     end
